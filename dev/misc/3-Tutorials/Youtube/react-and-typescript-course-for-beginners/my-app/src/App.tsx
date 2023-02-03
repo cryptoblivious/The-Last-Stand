0de47681334a4cbc0bd94cca2da814656f8fc6e1
cryptoblivious <andrzej.wis123@gmail.com>
2023-02-03 10:17:32 -0500

    Added Youtube tutorial material

---
+++ dev/misc/3-Tutorials/Youtube/react-and-typescript-course-for-beginners/my-app/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+
+const App:React.FC = () => {
+  return <div className="App">
+    <span className="heading">Taskify</span>
+    <InputField />
+    </div>;
+}
+
+export default App
