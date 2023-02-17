5d812563c8b9ef997ed406646b7ad8a1a46fe08a
cryptoblivious <andrzej.wis123@gmail.com>
2023-02-16 21:43:45 -0500

    Testing react router

--- dev/misc/tutorials/fi/tls/src/main.tsx
+++ dev/misc/tutorials/fi/tls/src/main.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RousterProvider, } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './styles/index.css';
 import { App, ErrorPage } from './components';
 import { Contact, Root } from './routes';
-import { loader as rootLoader } from './routes/root';
-
+import { loader as rootLoader, action as rootAction } from './routes/root';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    children: [
-      {
-        path: '/contacts/:contactId',
-        element: <Contact />,
-      },
-    ],
-  },
+	{
+		path: '/',
+		element: <Root />,
+		errorElement: <ErrorPage />,
+		loader: rootLoader,
+		action: rootAction,
+		children: [
+			{
+				path: '/contacts/:contactId',
+				element: <Contact />,
+			},
+		],
+	},
 ]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-    {/* <App /> */}
-  </React.StrictMode>,
+	<React.StrictMode>
+		<RouterProvider router={router} />
+		{/* <App /> */}
+	</React.StrictMode>
 );
--- dev/misc/tutorials/fi/tls/src/routes/root.tsx
+++ dev/misc/tutorials/fi/tls/src/routes/root.tsx
@@ -1,73 +1,75 @@
-import { 
-  Outlet as RouterOutlet,
-  Link ,
-  useLoaderData,
-} from "react-router-dom";
+import { Outlet as RouterOutlet, Link, useLoaderData, Form as RouterForm } from 'react-router-dom';
 
-import { getContacts } from "../components/contacts";
+import { getContacts, createContact } from '../components/contacts';
+
+export async function action() {
+	const contact = await createContact();
+	return { contact };
+}
 
 export async function loader() {
-  const contacts = await getContacts();
-  return { contacts };
+	const contacts = await getContacts();
+	return { contacts };
 }
 
 export default function Root() {
-  const { contacts } = useLoaderData();
-    return (
-      <>
-        <div id="sidebar">
-          <h1>React Router Contacts</h1>
-          <div>
-            <form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div
-                id="search-spinner"
-                aria-hidden
-                hidden={true}
-              />
-              <div
-                className="sr-only"
-                aria-live="polite"
-              ></div>
-            </form>
-            <form method="post">
-              <button type="submit">New</button>
-            </form>
-          </div>
-          <nav>
-          {contacts.length ? (
-            <ul>
-              {contacts.map((contact) => (
-                <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
-                    {contact.first || contact.last ? (
-                      <>
-                        {contact.first} {contact.last}
-                      </>
-                    ) : (
-                      <i>No Name</i>
-                    )}{" "}
-                    {contact.favorite && <span>★</span>}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>
-              <i>No contacts</i>
-            </p>
-          )}
-        </nav>
-        </div>
-        <div id="detail">
-          <RouterOutlet />
-        </div>
-      </>
-    );
-  }
+	const { contacts } = useLoaderData();
+	return (
+		<>
+			<div id="sidebar">
+				<h1>React Router Contacts</h1>
+				<div>
+					<form
+						id="search-form"
+						role="search">
+						<input
+							id="q"
+							aria-label="Search contacts"
+							placeholder="Search"
+							type="search"
+							name="q"
+						/>
+						<div
+							id="search-spinner"
+							aria-hidden
+							hidden={true}
+						/>
+						<div
+							className="sr-only"
+							aria-live="polite"></div>
+					</form>
+					<RouterForm method="post">
+						<button type="submit">New</button>
+					</RouterForm>
+				</div>
+				<nav>
+					{contacts.length ? (
+						<ul>
+							{contacts.map((contact) => (
+								<li key={contact.id}>
+									<Link to={`contacts/${contact.id}`}>
+										{contact.first || contact.last ? (
+											<>
+												{contact.first} {contact.last}
+											</>
+										) : (
+											<i>No Name</i>
+										)}{' '}
+										{contact.favorite && <span>★</span>}
+									</Link>
+								</li>
+							))}
+						</ul>
+					) : (
+						<p>
+							<i>No contacts</i>
+						</p>
+					)}
+				</nav>
+			</div>
+			<div id="detail">
+				<RouterOutlet />
+			</div>
+		</>
+	);
+}
