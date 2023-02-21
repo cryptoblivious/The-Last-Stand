693ce5d642514979d72278f323d3afd849e5ae06
cryptoblivious <andrzej.wis123@gmail.com>
2023-02-21 10:45:09 -0500

    Merged refs

--- dev/tutorials/tls/src/common/references.ts
+++ dev/tutorials/tls/src/common/references.ts
@@ -1,26 +1,38 @@
 export const references = [
 	{
-		id: 'reference-1',
+		id: 'ref-1',
 		link: 'https://nextjs.org/learn/foundations/',
 	},
 	{
-		id: 'reference-2',
+		id: 'ref-2',
 		link: 'https://www.youtube.com/watch?v=_oO4Qi5aVZs&t=1906s',
 	},
 	{
-		id: 'reference-3',
+		id: 'ref-3',
 		link: 'https://www.youtube.com/watch?v=FJDVKeh7RJI&t=3927s',
 	},
 	{
-		id: 'reference-4',
+		id: 'ref-4',
 		link: 'https://www.youtube.com/watch?v=w7ejDZ8SWv8&t=3996s',
 	},
 	{
-		id: 'reference-5',
+		id: 'ref-5',
 		link: 'https://reactrouter.com/en/main/start/tutorial',
 	},
 	{
-		id: 'reference-6',
+		id: 'ref-6',
 		link: 'https://www.youtube.com/watch?v=19TCO-GL6CA',
+	},
+	{
+		id: 'ref-7',
+		link: 'https://www.youtube.com/watch?v=OJxEcs0w_kE&t=119s',
+	},
+	{
+		id: 'ref-8',
+		link: 'https://phaser.io/docs/2.4.4/Phaser.QuadTree.html',
+	},
+	{
+		id: 'ref-9',
+		link: 'https://github.com/photonstorm/phaser/blob/v2.4.4/src/math/QuadTree.js',
 	},
 ];
