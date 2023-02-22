be3ac0f4c8547e7a75645c76bb200d904bf1c4d0
cryptoblivious <andrzej.wis123@gmail.com>
2023-02-22 15:47:22 -0500

    Worked on UML

--- dev/tutorials/mern/backend/models/Friend.ts
+++ dev/tutorials/mern/backend/models/Friend.ts
@@ -1,8 +1,39 @@
-import mongoose from 'mongoose';
+import { Schema, model } from 'mongoose';
 
-const friendSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
+const friendSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^\S+@\S+\.\S+$/,
     },
-    
+    username: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    isOnline: {
+      type: Boolean,
+      required: true,
+    },
+    lastOnline: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const friendModel = model('Friend', friendSchema);
+
+export default friendModel;
--- dev/tutorials/mern/backend/routes/friends.ts
+++ dev/tutorials/mern/backend/routes/friends.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import Friend from '../models/friend';
+
 const router = express.Router();
 
 // GET all friends
@@ -13,7 +15,23 @@ router.get('/:id', (req, res) => {
 });
 
 // POST a new friend
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  const { email, username, title, avatar, isOnline, lastOnline } = req.body;
+
+  try {
+    const friend = await Friend.create({
+      email,
+      username,
+      title,
+      avatar,
+      isOnline,
+      lastOnline,
+    });
+    res.status(200).json(friend);
+    res.json({ msg: 'Friend added' });
+  } catch (err: any) {
+    res.status(err.status || 500).json({ err: err.message || 'Unknown error' });
+  }
   res.json({ msg: 'POST a new friend' });
 });
 
