c3b9917827d1feb74e891e6378e5952e9197331e
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2023-02-26 07:31:02 -0500

    added dude

--- dev/tutorials/phaser-typescript-tuto-firstgame/src/FirstGame.ts
+++ dev/tutorials/phaser-typescript-tuto-firstgame/src/FirstGame.ts
@@ -1,19 +1,25 @@
 import Phaser from 'phaser'
+import Input from '../../vite/Jo/react-ts/src/components/Input';
 
 export default class FirstGame extends Phaser.Scene {
     private platforms? : Phaser.Physics.Arcade.StaticGroup
+    private player? : Phaser.Physics.Arcade.Sprite
+    private cursors? : Phaser.Types.Input.Keyboard.CursorKeys
+
 	constructor() {
 		super('first-game')
 	}
 
 	preload() {
-		//this.load.setBaseURL('https://labs.phaser.io')
 
 		this.load.image('background', 'assets/Background.png')
 		this.load.image('star', 'assets/star.png')
 		this.load.image('bomb', 'assets/bomb.png')
         this.load.image('ground', 'assets/platform.png')
-        this.load.image('dude', 'assets/dude.png')
+
+        // loading the spritesheet for the dude dont use add image
+        this.load.spritesheet('dude', 'assets/dude.png' , 
+        { frameWidth: 32, frameHeight: 48 })
 	}
 
 	create() {
@@ -33,7 +39,37 @@ export default class FirstGame extends Phaser.Scene {
         this.platforms.create(750, 220, 'ground')
 
 
+        this.player = this.physics.add.sprite(100, 450, 'dude')
+        this.player.setBounce(0.2)
+        // make the player collide with the bounds
+        this.player.setCollideWorldBounds(true)
 
+        // creating the animations for the dude and adding them to the scene
+        // we ll use them in the movements of the player
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+        })
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        })
+
+        this.physics.add.collider(this.player, this.platforms)
+
+        
+        this.cursors = this.input.keyboard.createCursorKeys()
 
 		// const particles = this.add.particles('star')
 
@@ -50,5 +86,34 @@ export default class FirstGame extends Phaser.Scene {
 		// logo.setCollideWorldBounds(true)
 
 		// emitter.startFollow(logo)
+
+        
 	}
+
+    update() {
+        if (!this.cursors) return
+
+        if (this.cursors.left?.isDown) {
+            this.player?.setVelocityX(-160)
+
+            this.player?.anims.play('left', true)
+        }
+
+        else if (this.cursors.right?.isDown) {
+            this.player?.setVelocityX(160)
+
+            this.player?.anims.play('right', true)
+        }
+        else{
+            this.player?.setVelocityX(0)
+
+            this.player?.anims.play('turn')
+        }
+
+        if (this.cursors.up?.isDown && this.player?.body.touching.down) {
+            this.player?.setVelocityY(-330)
+        }
+
+    }
+
 }
