b79d24c658b060be56d0e27e2417ae632e70764c
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2023-02-26 09:24:49 -0500

    first game done come and play andrzej

--- dev/tutorials/phaser-typescript-tuto-firstgame/src/FirstGame.ts
+++ dev/tutorials/phaser-typescript-tuto-firstgame/src/FirstGame.ts
@@ -1,10 +1,16 @@
 import Phaser from 'phaser'
-import Input from '../../vite/Jo/react-ts/src/components/Input';
 
 export default class FirstGame extends Phaser.Scene {
     private platforms? : Phaser.Physics.Arcade.StaticGroup
     private player? : Phaser.Physics.Arcade.Sprite
     private cursors? : Phaser.Types.Input.Keyboard.CursorKeys
+    private stars? : Phaser.Physics.Arcade.Group
+    private bombs? : Phaser.Physics.Arcade.Group
+
+    private score = 0
+    private scoreText? : Phaser.GameObjects.Text
+    private gameOver = false
+    
 
 	constructor() {
 		super('first-game')
@@ -67,9 +73,36 @@ export default class FirstGame extends Phaser.Scene {
         })
 
         this.physics.add.collider(this.player, this.platforms)
+        
 
         
         this.cursors = this.input.keyboard.createCursorKeys()
+
+        this.stars = this.physics.add.group({
+            key: 'star',
+            repeat: 11,
+            setXY: { x: 12, y: 0, stepX: 70 }
+        })
+
+        this.stars.children.iterate(c  => {
+            // type convertion/assertion sinon il chiale 
+            const child = c as Phaser.Physics.Arcade.Image
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8))
+        })
+
+        this.physics.add.collider(this.stars, this.platforms)
+        this.physics.add.overlap(this.player, this.stars, this.handleCollectStar, undefined, this)
+
+        this.scoreText = this.add.text(16, 16, 'score: 0', { 
+            fontSize: '32px', 
+            fill: '#000' 
+        })
+
+        this.bombs = this.physics.add.group()
+        this.physics.add.collider(this.bombs, this.platforms)
+        this.physics.add.collider(this.player, this.bombs, this.handleHitBomb, undefined, this)
+        
+
 
 		// const particles = this.add.particles('star')
 
@@ -86,14 +119,55 @@ export default class FirstGame extends Phaser.Scene {
 		// logo.setCollideWorldBounds(true)
 
 		// emitter.startFollow(logo)
-
-        
+     
 	}
+
+    private handleHitBomb(player: Phaser.GameObjects.GameObject, b: Phaser.GameObjects.GameObject) {
+        //type  casting sinon il chiale
+        const bomb = b as Phaser.Physics.Arcade.Image
+        this.physics.pause()
+
+        this.player?.setTint(0xff0000)
+        this.player?.anims.play('turn')
+        this.gameOver = true
+        
+    }
+
+    // this is a private method that will be called when the player overlaps with a star
+    private handleCollectStar(player: Phaser.GameObjects.GameObject, s: Phaser.GameObjects.GameObject) {
+        //type  casting sinon il chiale 
+        const star = s as Phaser.Physics.Arcade.Image
+        star.disableBody(true, true)
+        this.score += 10
+        this.scoreText?.setText(`Score: ${this.score}`)
+
+        if (this.stars?.countActive(true) === 0) {
+            this.stars.children.iterate(c  => {
+                // type convertion/assertion sinon il chiale 
+                const child = c as Phaser.Physics.Arcade.Image
+                child.enableBody(true, child.x, 0, true, true)
+            })
+
+            if (this.player ){
+                const x = this.player?.x < 400 
+                ? Phaser.Math.Between(400, 800) 
+                : Phaser.Math.Between(0, 400)
+
+                const bomb = this.bombs?.create(x, 16, 'bomb')
+                bomb.setBounce(1)
+                bomb.setCollideWorldBounds(true)
+                bomb.setVelocity(Phaser.Math.Between(-200, 200), 20)
+            }
+            
+
+        }
+
+    }
 
     update() {
         if (!this.cursors) return
 
-        if (this.cursors.left?.isDown) {
+        if (this.cursors.left?.isDown ) {
             this.player?.setVelocityX(-160)
 
             this.player?.anims.play('left', true)
@@ -110,6 +184,7 @@ export default class FirstGame extends Phaser.Scene {
             this.player?.anims.play('turn')
         }
 
+        // jump if the player is touching the ground
         if (this.cursors.up?.isDown && this.player?.body.touching.down) {
             this.player?.setVelocityY(-330)
         }
@@ -117,3 +192,4 @@ export default class FirstGame extends Phaser.Scene {
     }
 
 }
+
