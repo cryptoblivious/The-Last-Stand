831d6e4f97668a9e48c3a71155060bf53707e31a
cryptoblivious <andrzej.wis123@gmail.com>
2023-02-27 20:13:01 -0500

    Working on route controllers

--- dev/tutorials/mern/backend/routes/friends.ts
+++ dev/tutorials/mern/backend/routes/friends.ts
@@ -1,39 +1,16 @@
 import express from 'express';
-
-import Friend from '../models/friend';
+import { createFriend, getFriends, getFriend } from '../controllers/friends';
 
 const router = express.Router();
 
 // GET all friends
-router.get('/', (req, res) => {
-  res.json({ msg: 'GET all friends' });
-});
+router.get('/', getFriends);
 
 // GET one friend
-router.get('/:id', (req, res) => {
-  res.json({ msg: 'GET one friend' });
-});
+router.get('/:email', getFriend);
 
 // POST a new friend
-router.post('/', async (req, res) => {
-  const { email, username, title, avatar, isOnline, lastOnline } = req.body;
-
-  try {
-    const friend = await Friend.create({
-      email,
-      username,
-      title,
-      avatar,
-      isOnline,
-      lastOnline,
-    });
-    res.status(200).json(friend);
-    res.json({ msg: 'Friend added' });
-  } catch (err: any) {
-    res.status(err.status || 500).json({ err: err.message || 'Unknown error' });
-  }
-  res.json({ msg: 'POST a new friend' });
-});
+router.post('/', createFriend);
 
 // UPDATE a friend
 router.patch('/:id', (req, res) => {
