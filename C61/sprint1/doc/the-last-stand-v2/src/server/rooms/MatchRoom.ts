e544171bc54f9ff74721544f41a6075300fc0f0b
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2023-03-05 00:13:57 -0500

    event se rend pas au client

--- C61/sprint1/doc/the-last-stand-v2/src/server/rooms/MatchRoom.ts
+++ C61/sprint1/doc/the-last-stand-v2/src/server/rooms/MatchRoom.ts
@@ -1,12 +1,24 @@
-import { Room } from "colyseus";
+import { Client, Room } from "colyseus";
 import { MatchRoomState } from "./schema/MatchRoomState";
+import { Message } from "../../types/messages";
+import { Dispatcher } from "@colyseus/command";
+import PlayerSelectionCommand from "../commands/PlayerSelectionCommands";
 
 // export default didnt work here but classic export did
 export class MatchRoom extends Room<MatchRoomState> {
 
-    onCreate(){
+    private dispatcher = new Dispatcher(this)
+
+    onCreate() {
         // set initial room state
         this.setState(new MatchRoomState())
+
+        this.onMessage(Message.PlayerSelection, (client, message) => {
+            this.dispatcher.dispatch(new PlayerSelectionCommand(), {
+                client: client,
+                index: message.index
+            });
+        })
     }
 
 }
