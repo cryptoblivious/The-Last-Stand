08752b7b18ad09c03b186818e7acd23824407c98
cryptoblivious <andrzej.wis123@gmail.com>
2023-03-13 15:11:43 -0400

    Direct import of components

--- dev/the-last-stand/src/client/main.tsx
+++ dev/the-last-stand/src/client/main.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import { Route, createBrowserRouter, RouterProvider, createRoutesFromElements } from 'react-router-dom';
-import { Error, Home, Lobby, Login, Match } from './components';
 import { ProtectedRoute } from './routes/components/ProtectedRoute';
+import Error from './components/Error';
+import Home from './components/Home';
+import Lobby from './components/Lobby';
+import Login from './components/Login';
+import Match from './components/Match';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
