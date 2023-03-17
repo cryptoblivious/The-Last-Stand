b848042d57e70ac09d856f119a8041f2cb5c5b3d
cryptoblivious <andrzej.wis123@gmail.com>
2023-03-17 16:36:28 -0400

    Trying to make SSL work

--- dev/the-last-stand/src/server/main.ts
+++ dev/the-last-stand/src/server/main.ts
@@ -1,3 +1,5 @@
+import https from 'https';
+import fs from 'fs';
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
@@ -20,15 +22,21 @@ import usersRouter from './routes/users';
 mongoose.set('strictQuery', false);
 dotenv.config();
 
-const { MONGO_URI, SESSION_SECRET, CLIENT_URL, CLIENT_PORT, HOST_PORT } = process.env as Record<string, string>;
-
-//const mongoUri: string = process.env.MONGO_URI?.toString() ?? 'Invalid mongo uri';
-//const sessionSecret: string = process.env.SESSION_SECRET?.toString() ?? 'Invalid mongo uri';
+const { APP_MODE, MONGO_URI, SESSION_SECRET, CLIENT_URL, CLIENT_PORT, HOST_PORT } = process.env as Record<string, string>;
 
 const store = new MongoStore({
   mongoUrl: MONGO_URI,
   collectionName: 'sessions',
 });
+
+// Load SSL certificates and private keys if in production mode
+let sslOptions: any = {};
+if (APP_MODE === 'prod') {
+  sslOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/tls.woodchuckgames.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/tls.woodchuckgames.com/fullchain.pem'),
+  };
+}
 
 // Create an express app
 const app = express();
@@ -58,7 +66,6 @@ const corsOptions = {
 }; // REF : ChatGPT
 app.use(cors(corsOptions));
 app.use((req: any, res: { header: (arg0: string, arg1: string) => void }, next: () => void) => {
-  //res.header('Access-Control-Allow-Origin', 'https://tls.woodchuckgames.com, http://localhost:5173');
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
@@ -100,20 +107,18 @@ app.use('/colyseus', monitor());
 
 // Create a Colyseus server
 const gameServer = new Server({
-  //server: app.listen(9001),
   transport: new WebSocketTransport(),
 });
 
 // Define rooms here
 gameServer.define('match_orchestrator', MatchOrchestrator);
 
-// Attach the express instance to the Colyseus server
-gameServer.attach({ server: app.listen(HOST_PORT) });
-// Listen for incoming connections on the Colyseus server
-//gameServer.listen(9001);
+// Attach the express instance to the https server
+const server = https.createServer(sslOptions, app);
 
-// Listen to your remote server through the http server
-//httpServer.listen(9001, 'localhost', () => {
-// httpServer.listen(9001, '0.0.0.0', () => {
-//   console.log('Listening to remote server 54.210.205.37 on port 9001');
-// });
+// Attach the express instance to the Colyseus server
+if (APP_MODE === 'dev') {
+  gameServer.attach({ server: app.listen(HOST_PORT) });
+} else {
+  gameServer.attach({ server: server.listen(HOST_PORT) });
+}
