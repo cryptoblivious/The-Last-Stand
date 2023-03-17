401b7ce51373d076c1da1695d4caab67391d1bf5
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2023-03-17 15:17:44 -0400

    having fun with tailwind CSS

---
+++ C61/doc/react-components/src/components/Card.tsx
@@ -0,0 +1,43 @@
+const cardCSS = 'bg-cover bg-center bg-no-repeat rounded-lg cursor-pointer overflow-hidden shadow-md flex justify-center items-center hover:scale-110 transition-all durantion-500 ease-in-out'
+const selectedCardCSS = 'border-2 border-blue-500'
+const cardTextCSS = 'text-2xl text-white text-center bg-transparent'
+
+interface Card {
+    id: number;
+    reference: string;
+    image: string;
+
+}
+
+interface CardProps {
+    card: Card;
+    isSelected: boolean;
+    onClick: () => void;
+    width?: string;
+    height?: string;
+}
+
+
+const Card = ({ card, isSelected, onClick, width = "400px", height = "250px" }: CardProps) => {
+    return (
+        <div         
+            className={`${cardCSS} ${isSelected ? selectedCardCSS : ''}`}
+            style=
+            {{
+                backgroundImage: `url(${card.image})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                width,
+                height
+            }}
+            onClick={onClick}
+        >
+            <div className={cardTextCSS}>
+                {card.reference}
+            </div>
+        </div>
+    );
+};
+
+export default Card;
