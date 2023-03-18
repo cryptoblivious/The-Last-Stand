c55a608bbfe4cd32eaf582d4bfe43b36f89a78a1
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2023-03-17 21:57:25 -0400

    card selected and working on arrows

--- C61/doc/react-components/src/components/Card.tsx
+++ C61/doc/react-components/src/components/Card.tsx
@@ -1,7 +1,6 @@
 const cardCSS = 'bg-cover bg-center bg-no-repeat rounded-lg cursor-pointer overflow-hidden shadow-md flex justify-center items-center hover:scale-110 transition-all durantion-500 ease-in-out'
 const selectedCardCSS = 'border-2 border-blue-500'
 const cardTextCSS = 'text-2xl text-white text-center bg-transparent'
-
 interface Card {
     id: number;
     reference: string;
@@ -17,9 +16,10 @@ interface ICardProps {
 }
 
 const Card = ({ card, isSelected, onClick, width = 400, height = 250 }: ICardProps) => {
+    const cardOpacityCSS = isSelected ? '' : 'opacity-30'
     return (
         <div         
-            className={`${cardCSS} ${isSelected ? selectedCardCSS : ''}`}
+            className={`${cardCSS} ${isSelected ? selectedCardCSS : ''} ${cardOpacityCSS}`}
             style=
             {{
                 backgroundImage: `url(${card.image})`,
--- C61/doc/react-components/src/components/CardMenu.tsx
+++ C61/doc/react-components/src/components/CardMenu.tsx
@@ -2,7 +2,7 @@ import Card from './Card';
 import { useState } from 'react';
 
 const cardMenuContainerCSS = 'flex-col justify-center items-center gap-4 space-y-4';
-
+const cardMenuButtonCSS = 'focus:outline-none';
 export interface CardMenuCard {
     id: number;
     name: string;
@@ -19,28 +19,50 @@ const getOddNumber = (number: number) => {
 };
 
 const CardMenu = ({
-    heroes, visibleCards = 3}: ICardMenuProps) => {
-    const [selectedHeroId, setSelectedHeroId] = useState<number | null>(null);
-    const [carrouselStartIndex, setCarrouselStartIndex] = useState(0);
+    heroes, visibleCards = 3 }: ICardMenuProps) => {
 
+    // make sure the number of visible cards is odd
     visibleCards = getOddNumber(visibleCards);
+    // get the index of the center card
     const centerCardIndex = Math.floor(visibleCards / 2);
 
+    // set the selected hero id to the center card
+    const [selectedHeroId, setSelectedHeroId] = useState(heroes[centerCardIndex].id);
+    const [carrouselStartIndex, setCarrouselStartIndex] = useState(0);
+
     const handleCardClick = (id: number, index: number) => {
+        // update selected hero id
         setSelectedHeroId(id);
 
         //update carrousel start index when clicking on a card
         if (index !== centerCardIndex) {
-
             setCarrouselStartIndex((prevIndex) => {
+                // get the new index
                 const newIndex = prevIndex + (index - centerCardIndex);
+                // if the new index is negative, add the length of the array to it else get the modulo
                 return newIndex < 0 ? heroes.length + newIndex : newIndex % heroes.length;
             });
         }
     };
 
+    // go to the previous or next card according to the isPrevious boolean
+    const goToCard = (isPrevious:boolean) => {
+        setCarrouselStartIndex((prevIndex) => {
+            //
+            const newIndex = isPrevious ?  prevIndex - 1 : prevIndex + 1;
+            const updateIndex = newIndex < 0 ? heroes.length + newIndex : newIndex % heroes.length;
+            const selectedHeroIndex = (updateIndex + centerCardIndex) % heroes.length;
+
+            // if the selected hero index is valid, update the selected hero id
+            if (selectedHeroIndex >= 0 && selectedHeroIndex < heroes.length){
+                setSelectedHeroId(heroes[selectedHeroIndex].id);
+            }
+            return updateIndex;
+        });
+    };
+
     const getVisibleHeroes = (startIndex: number) => {
-        // get the visible cards
+        // get the wanted visible cards
         let visibleHeroes = [];
         // if the start index is negative, add the length of the array to it
         for (let i = 0; i < visibleCards; i++) {
@@ -53,7 +75,10 @@ const CardMenu = ({
 
     return (
         <div className={cardMenuContainerCSS}>
-            {/* use our function to get the array of wanted visible cards */}
+            <button className={cardMenuButtonCSS} onClick={() => goToCard(true)}>
+                Previous
+            </button>
+            {/* use our function to get the array of wanted visible cards and map throught it to render them*/}
             {getVisibleHeroes(carrouselStartIndex).map((hero, index) => (
                 <Card
                     key={hero.id}
@@ -62,6 +87,9 @@ const CardMenu = ({
                     onClick={() => handleCardClick(hero.id, index)}
                 />
             ))}
+            <button className={cardMenuButtonCSS} onClick={() => goToCard(false)}>
+                Next
+            </button>
         </div>
     );
 };
