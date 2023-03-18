bb0ec3b9273be157c9f53c25aa4339731a042410
cryptoblivious <andrzej.wis123@gmail.com>
2023-03-18 12:59:55 -0400

    Removed TLS folder in sprint1 folder

--- C61/sprint1/doc/the-last-stand/node_modules/@colyseus/loadtest/bin.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-const path = require('path');
-
-require('ts-node').register({
-  ignore: [],
-  project: process.env.TS_NODE_PROJECT || path.resolve(__dirname,  'tsconfig.json')
-});
-
-require('./build');
