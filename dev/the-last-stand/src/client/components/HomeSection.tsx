690b5bf2a8825bb1ce9611cc210884c502d914cd
cryptoblivious <andrzej.wis123@gmail.com>
2023-03-19 00:00:28 -0400

    Overlay functionality looks pretty awesome

--- dev/the-last-stand/src/client/components/HomeSection.tsx
+++ dev/the-last-stand/src/client/components/HomeSection.tsx
@@ -17,17 +17,17 @@ const HomeSection = (props: IHomeSectionProps) => {
   const { title, subtitle, link, className, backgroundImg } = props;
   return (
     <div
-      className={`${className ?? 'gap-10 h-full rounded-3xl flex flex-col flex-grow items-center justify-center border-transparent hover:border-purple-900 hover:scale-105 transition ease-in-out duration-1000 border-4 transform'}${backgroundImg && 'bg-no-repeat bg-cover'}`}
+      className={`${className ?? 'gap-10 h-full rounded-3xl flex flex-col flex-grow items-center justify-end pb-10 border-transparent hover:border-purple-900 hover:scale-105 transition ease-in-out duration-1000 border-4 transform'}${backgroundImg && 'bg-no-repeat bg-cover'}`}
       style={backgroundImg ? { backgroundImage: `url(${backgroundImg})` } : {}}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}>
-      <h1>{title}</h1>
-      <div className={`${isHovered ? 'h-10' : 'h-0'} transition-all duration-300 ease-out`}>
+      <div className={`${isHovered ? 'bg-opacity-60 h-32' : 'bg-opacity-0'} bg-black flex flex-col justify-evenly items-center rounded-3xl px-5 py-2 transition-all duration-1000 ease-out`}>
+        <h1>{title}</h1>
         <p className={`${isHovered ? 'opacity-100' : 'opacity-0'} overflow-y-hidden transition-all ease-in duration-500`}>{subtitle}</p>
       </div>
       <Link to={link}>
         <Button
-          className='w-16 bg-purple-900 rounded-xl p-1 border-4 border-fuchsia-700 hover:bg-fuchsia-700 hover:border-purple-900 transition ease-in-out duration-1000 hover:scale-110'
+          className='w-32 h-24 bg-purple-900 rounded-xl p-1 border-4 border-fuchsia-700 hover:bg-fuchsia-700 hover:border-purple-900 transition ease-in-out duration-1000 hover:scale-110'
           text='Go'
         />
       </Link>
--- dev/the-last-stand/src/client/components/SocialOverlay.tsx
+++ dev/the-last-stand/src/client/components/SocialOverlay.tsx
@@ -2,10 +2,28 @@ import SendFriendRequestForm from './SendFriendRequestForm';
 import UserInfoCard from './UserInfoCard';
 import FriendList from './FriendList';
 import Button from './Button';
+import { useEffect, useState } from 'react';
 
 const SocialOverlay = () => {
+  const [overlaying, setOverlaying] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        setOverlaying(false);
+      } else if (e.key === 'o') {
+        e.preventDefault();
+        setOverlaying(!overlaying);
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [overlaying]);
+
   return (
-    <div className='z-40 bg-black overflow-y-scroll text-pink-600 border-pink-600 border-2 p-3 pr-12 rounded-3xl border-r-0 rounded-r-none w-1/4 flex flex-col fixed top-2 right-0 bottom-2 gap-5'>
+    <div className={`${overlaying ? '-right-5' : '-right-96'} transition-all duration-1000 z-40 bg-black overflow-y-scroll text-pink-600 border-pink-600 border-2 p-3 pr-12 rounded-3xl border-r-0 rounded-r-none w-1/4 flex flex-col fixed top-2 bottom-2 gap-5 `}>
       <UserInfoCard />
       <SendFriendRequestForm />
       <FriendList />
