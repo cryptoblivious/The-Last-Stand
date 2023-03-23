40fc6d5d9ec4cbc549ade5b4360233039f93db41
cryptoblivious <andrzej.wis123@gmail.com>
2023-03-23 15:58:14 -0400

    changed import path

--- dev/the-last-stand/src/client/fetches/heroes.ts
+++ dev/the-last-stand/src/client/fetches/heroes.ts
@@ -1,9 +1,9 @@
 import { HOST_URL, HOST_PORT } from '../appConfig';
 import IHeroMapCard from '../../typescript/interfaces/IHeroMapCard';
 import { capitalizeFirstLetter } from '../../utils/text_format';
-import solana from '../../assets/heroes/solana/portrait.webp';
-import logan from '../../assets/heroes/logan/portrait.png';
-import chuck from '../../assets/heroes/chuck/portrait.png';
+import solana from '../assets/heroes/solana/portrait.webp';
+import logan from '../assets/heroes/logan/portrait.png';
+import chuck from '../assets/heroes/chuck/portrait.png';
 
 // Record of hero names and their portrait images, should be moved to a config file
 const heroImages: Record<string, string> = {
