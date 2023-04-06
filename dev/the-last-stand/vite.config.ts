e32a3da1b7024486b1ffbdab1223e887f6cbc5b6
cryptoblivious <andrzej.wis123@gmail.com>
2023-04-06 14:46:27 -0400

    tried modifying copy values in vite config

--- dev/the-last-stand/vite.config.ts
+++ dev/the-last-stand/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     copy({
       targets: [
         // copy all files inside the assets folder
-        { src: 'src/client/assets/**/*', dest: `dist${ASSETS_BASE_PATH}` },
+        { src: 'dev/the-last-stand/src/client/assets/**/*', dest: `dev/the-last-stand/dist${ASSETS_BASE_PATH}` },
       ],
       // make sure that the copy plugin runs before the build starts
       // to include the assets files before they are needed in the app
