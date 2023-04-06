42ea98baa405b914c8ce4c408981bd29c931e262
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2023-04-06 16:38:22 -0400

    commit

---
+++ dev/the-last-stand/public/assets/index.ts
@@ -0,0 +1,29 @@
+// import login from './assets/wallpapers/login.png';
+
+// import chuckIdlePath from './heroes/chuck/spritesheets/chuck_idle.png';
+// import chuckRunPath from './heroes/chuck/spritesheets/chuck_run.png';
+// import chuckJumpPath from './heroes/chuck/spritesheets/chuck_jump.png';
+// import chuckAttackPath1 from './heroes/chuck/spritesheets/chuck_attack_1.png';
+// import chuckAttackPath2 from './heroes/chuck/spritesheets/chuck_attack_2.png';
+// import chuckAttackPath3 from './heroes/chuck/spritesheets/chuck_attack_3.png';
+// import chuckClimbPath from './heroes/chuck/spritesheets/chuck_climb.png';
+// import chuckDeathPath from './heroes/chuck/spritesheets/chuck_death.png';
+// import chuckHurtPath from './heroes/chuck/spritesheets/chuck_hurt.png';
+// import chuckDoubleJumpPath from './heroes/chuck/spritesheets/chuck_doublejump.png';
+// import chuckKickPath from './heroes/chuck/spritesheets/chuck_kick.png';
+// import chuckRunAttackPath from './heroes/chuck/spritesheets/chuck_run_attack.png';
+
+// import solanaIdlePath from './heroes/solana/spritesheets/solana_idle.png';
+// import solanaRunPath from './heroes/solana/spritesheets/solana_run.png';
+// import solanaJumpPath from './heroes/solana/spritesheets/solana_jump.png';
+// import solanaAttackPath1 from './heroes/solana/spritesheets/solana_attack_1.png';
+// import solanaAttackPath2 from './heroes/solana/spritesheets/solana_attack_2.png';
+// import solanaAttackPath3 from './heroes/solana/spritesheets/solana_attack_3.png';
+// import solanaClimbPath from './heroes/solana/spritesheets/solana_climb.png';
+// import solanaDeathPath from './heroes/solana/spritesheets/solana_death.png';
+// import solanaHurtPath from './heroes/solana/spritesheets/solana_hurt.png';
+// import solanaDoubleJumpPath from './heroes/solana/spritesheets/solana_doublejump.png';
+// import solanaPunchPath from './heroes/solana/spritesheets/solana_punch.png';
+// import solanaRunAttackPath from './heroes/solana/spritesheets/solana_run_attack.png';
+
+// export { login, chuckIdlePath, chuckRunPath, chuckJumpPath, chuckAttackPath1, chuckAttackPath2, chuckAttackPath3, chuckClimbPath, chuckDeathPath, chuckHurtPath, chuckDoubleJumpPath, chuckKickPath, chuckRunAttackPath, solanaIdlePath, solanaRunPath, solanaJumpPath, solanaAttackPath1, solanaAttackPath2, solanaAttackPath3, solanaClimbPath, solanaDeathPath, solanaHurtPath, solanaDoubleJumpPath, solanaPunchPath, solanaRunAttackPath };
