eea4405a3cfc0ae6b1eea4e515edcaf47347624f
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2023-04-14 11:19:11 -0400

    working on backgrounds

---
+++ dev/the-last-stand/src/tiled/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tiles" tilewidth="32" tileheight="32" tilecount="81" columns="9">
+ <image source="../../../../../../Downloads/Tiles.jpg" width="288" height="288"/>
+</tileset>
