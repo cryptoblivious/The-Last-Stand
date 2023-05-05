ed62eeae5b76ffe5e8f0103530301881cadb123f
cryptoblivious <andrzej.wis123@gmail.com>
2023-05-04 21:55:00 -0400

    Made userMapperId optionnal because of guests

--- dev/the-last-stand/src/typescript/interfaces/IUserMapper.ts
+++ dev/the-last-stand/src/typescript/interfaces/IUserMapper.ts
@@ -1,7 +1,7 @@
 import { Schema, type } from '@colyseus/schema';
 
 export class IUserMapper extends Schema {
-  @type('string') _id: string = '';
+  @type('string') _id?: string = '';
   @type('string') username?: string = '';
   @type('string') userNo?: string = '';
   @type('string') clientId: string = '';
