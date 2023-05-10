d68b10fb59cc906c14a60139c54e282a13159bfa
cryptoblivious <andrzej.wis123@gmail.com>
2023-05-10 15:24:55 -0400

    Cron job should put reset message in the global chat

--- dev/the-last-stand/src/server/cron/controllers/database.ts
+++ dev/the-last-stand/src/server/cron/controllers/database.ts
@@ -1,6 +1,7 @@
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 import { conversationModel as Conversation } from '../../api/models/conversation';
+import { IMessageMapper } from '../../../typescript/interfaces/IMessageMapper';
 dotenv.config();
 const { MONGO_URI } = process.env;
 
@@ -9,7 +10,11 @@ export async function emptyGlobalChatMessages() {
     const client = new MongoClient(MONGO_URI!);
     await client.connect();
     const globalChat = await Conversation.findOne({ isGlobal: true });
-    await Conversation.findOneAndUpdate({ _id: globalChat._id }, { $set: { messages: [] } });
+    const resetMessage = new IMessageMapper();
+    resetMessage.content = 'Messages have been reset at ' + new Date().toLocaleString(undefined, { dateStyle: 'full', timeStyle: 'medium' });
+    resetMessage.userNo = '0000';
+    resetMessage.username = 'Server';
+    await Conversation.findOneAndUpdate({ _id: globalChat._id }, { $set: { messages: [resetMessage] } });
     client.close();
     console.log('global chat messages emptied');
   } catch (err) {
