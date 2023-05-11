6b3a9150d47084ff556fa82af7f67648940d182d
cryptoblivious <andrzej.wis123@gmail.com>
2023-05-11 13:37:58 -0400

    Added appropriate icons to overlay toggler

--- dev/the-last-stand/src/client/components/OverlayToggler.tsx
+++ dev/the-last-stand/src/client/components/OverlayToggler.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Button from './Button';
-import { GiFeather, GiBookmarklet } from 'react-icons/gi';
+import { GiNinjaHead, GiBellShield } from 'react-icons/gi';
 
 interface IToggleOverlayProps {
   onClick: () => void;
@@ -9,7 +9,7 @@ interface IToggleOverlayProps {
 const OverlayToggler = ({ onClick }: IToggleOverlayProps) => {
   const toggleOverlay = onClick;
   const [active, setActive] = useState<boolean>(false);
-  const icon = active ? 'GiBookmarklet' : 'GiFeather';
+  const icon = active ? 'GiNinjaHead' : 'GiBellShield';
 
   const toggleActive = () => {
     setActive((prev) => !prev);
@@ -21,15 +21,15 @@ const OverlayToggler = ({ onClick }: IToggleOverlayProps) => {
       className='z-50 bg-pink-600 text-white border-purple-900 border-4 rounded-full h-14 w-14 fixed transition duration-500 flex justify-center items-center hover:scale-150 right-0 top-0'
       onClick={toggleActive}
       icon={
-        icon === 'GiFeather' ? (
-          <GiFeather
-            aria-label='GiFeather'
+        icon === 'GiNinjaHead' ? (
+          <GiNinjaHead
+            aria-label='GiNinjaHead'
             fontSize='1.69rem'
             color='rgb(103 232 249)'
           />
         ) : (
-          <GiBookmarklet
-            aria-label='GiBookmarklet'
+          <GiBellShield
+            aria-label='GiBellShield'
             fontSize='1.69rem'
             color='rgb(103 232 249)'
           />
--- dev/the-last-stand/src/server/cron/configs/database.ts
+++ dev/the-last-stand/src/server/cron/configs/database.ts
@@ -2,7 +2,7 @@ import { emptyGlobalChatMessages } from '../controllers/database';
 
 // Define cron job configuration object
 export const cleanGlobalChat = {
-  cronTime: '0 0 2 * * *',
+  cronTime: '0 35 13 * * *',
   onTick: emptyGlobalChatMessages,
   timeZone: 'America/New_York',
   start: true, // Start the job automatically
