2fbe721fa77cce9594f111b8af6104980c059970
cryptoblivious <andrzej.wis123@gmail.com>
2023-05-25 10:10:44 -0400

    Conversation title now changes when username change

--- dev/the-last-stand/src/server/api/controllers/users.ts
+++ dev/the-last-stand/src/server/api/controllers/users.ts
@@ -5,6 +5,7 @@
 //  Références : https://chat.openai.com/, https://www.youtube.com/watch?v=98BzS5Oz5E4
 
 import { userModel as User } from '../models/user';
+import { conversationModel as Conversation } from '../models/conversation';
 import { findUniqueNumber, formatNumber, unformatNumbers } from '../../../utils/maths/maths';
 
 export const findAvailableUsernameNumber = async (username: string) => {
@@ -54,6 +55,39 @@ export const readUserByEmail = async (req: any, res: any) => {
 };
 
 export const patchCurrentUser = async (req: any, res: any) => {
+  const updateUserConversations = async (user: any) => {
+    const userConversations = await Conversation.find({
+      $or: [{ userIds: user._id }, { isGlobal: true }],
+    });
+    userConversations.forEach(async (conversation: any) => {
+      const users = conversation.userIds.map(async (userId: string) => {
+        const user = await User.findById(userId);
+        return user;
+      });
+      const resolvedUsers = await Promise.all(users);
+      const updatedNames: string[] = resolvedUsers.map((resolvedUser: any) => {
+        if (resolvedUser._id.toString() === user._id.toString()) {
+          return user.username;
+        } else {
+          return resolvedUser.username;
+        }
+      });
+      const updatedName = !conversation.isGlobal ? `${updatedNames.join(' and ')}'s Chat` : conversation.name;
+      const updatedMessages = conversation.messages.map((message: any) => {
+        if (message.userId === user._id.toString()) {
+          message.username = user.username;
+          message.userNo = user.userNo;
+        }
+        return message;
+      });
+      try {
+        await Conversation.findByIdAndUpdate(conversation._id, { messages: updatedMessages, name: updatedName });
+      } catch (err: any) {
+        console.log('err', err);
+      }
+    });
+  };
+
   const id = req.user?._id;
   if (!id) {
     return res.status(400).json({ err: 'Invalid user ID' });
@@ -62,7 +96,7 @@ export const patchCurrentUser = async (req: any, res: any) => {
     if (req.body.username) {
       const userWithSameName = await User.findOne({ username: req.body.username });
       if (userWithSameName && id !== userWithSameName._id.toString()) {
-        const availableUsernameNo: string = await findAvailableUsernameNumber(req.body.username);
+        const availableUsernameNo = await findAvailableUsernameNumber(req.body.username);
         if (availableUsernameNo === '-1') {
           return res.status(400).json({ err: `No available username number associated with ${req.body.username}` });
         }
@@ -75,6 +109,7 @@ export const patchCurrentUser = async (req: any, res: any) => {
     if (!user) {
       return res.status(404).json({ err: 'User not found' });
     }
+    await updateUserConversations(user);
     res.status(200).json(user);
   } catch (err: any) {
     res.status(err.status || 500).json({ err: err.message || 'Unknown error' });
