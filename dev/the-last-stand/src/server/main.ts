d1fb1b05e505e5fa3e6968359a36c333c9ad5a66
cryptoblivious <andrzej.wis123@gmail.com>
2023-08-24 14:16:45 -0400

    Added andrzejw.com to CORS whitelist

--- dev/the-last-stand/src/server/main.ts
+++ dev/the-last-stand/src/server/main.ts
@@ -67,7 +67,7 @@ if (APP_MODE === 'prod') {
   console.log('✅ SSL certificates loaded.');
 }
 
-const whitelist = ['https://tls.woodchuckgames.com', 'http://localhost:5173'];
+const whitelist = ['https://tls.woodchuckgames.com', 'https://tls.andrzejw.com', 'http://localhost:5173'];
 const corsOptions = {
   origin: function (origin: any, callback: any) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
